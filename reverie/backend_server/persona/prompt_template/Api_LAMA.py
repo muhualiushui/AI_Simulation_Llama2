925c4833f72ca6b77cdc71fbde226746e2d6e9c4
muhualiushui <shuzhen2@illinois.edu>
2023-10-05 01:43:36 -0500

    Added code

---
+++ reverie/backend_server/persona/prompt_template/Api_LAMA.py
@@ -0,0 +1,22 @@
+import json
+import requests
+
+def send(query):
+    try:
+        url = "https://llama.k8s-gosha.atlas.illinois.edu/completion" 
+        myobj = {
+            "prompt": query,
+            "n_predict": 128
+        }
+
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Basic YXRsYXNhaXRlYW06anhAVTJXUzhCR1Nxd3U="
+        }
+
+        response = requests.post(url, data=json.dumps(myobj), headers=headers, 
+                                auth=('atlasaiteam', 'jx@U2WS8BGSqwu'), timeout=10)
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+    return response.json()
